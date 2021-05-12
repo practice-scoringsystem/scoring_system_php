167d3cd31551146d9645e830312ed872bbdfe8e0
hayashi-3 <hayashi.gml@gmail.com>
2021-05-12 14:11:43 +0900

    [add]score-function

--- public/test_result.php
+++ public/test_result.php
@@ -6,8 +6,12 @@
   $question_ids = $_POST['ids'];
   $inp_answers = $_POST['input_answers'];
 
+  // $ans_arr = array();
+  $score = 0;
+
   // DBの値を取り出す
   for($i = 0; $i < count($question_ids); $i++) {
+
     $q_id = (int)$question_ids[$i];
     $ca = new CorrectAnswers();
     $db_data = $ca->getAnsByQuestionsId($q_id);
@@ -15,35 +19,17 @@
     // 取り出したDBの値から必要な情報だけ取り出す
     $answers = array_column($db_data, 'answer');
 
-    echo "<pre>";
-      var_dump($answers);
-    echo "<pre>";
-
     // フォームから入力した値を回す
     for($j = 0; $j < count($inp_answers); $j++) {
       $inp_ans = $inp_answers[$j];
-
-      $score = 0;
 
       // DBの値を回す
       for($k = 0; $k < count($answers); $k++) {
         $db_ans = $answers[$k];
         
         if ($db_ans === $inp_ans) {
-
-          echo "<pre>";
-            var_dump($db_ans);
-          echo "</pre>";
-          echo "<pre>";
-            var_dump($inp_ans);
-          echo "<pre>";
-
           $score++;
-          echo "一致\n";
           break;
-          
-        } else {
-          echo "不一致\n";
         }
       }
 
@@ -51,6 +37,27 @@
 
   }
 
-  echo($score);
+  // 問題数のカウント
+  $q_count = count($question_ids);
+
+  // 採点
+  $result = round(100 * $score / $q_count);
 
 ?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>テスト結果</title>
+</head>
+<body>
+  <?php include("./common/header.php"); ?>
+  <?php
+    // php.iniで上手く設定できなかったためtimezoneを設定
+    date_default_timezone_set('Asia/Tokyo');
+    echo date("Y/m/d H:i:s")
+  ?>
+</body>
+</html>
